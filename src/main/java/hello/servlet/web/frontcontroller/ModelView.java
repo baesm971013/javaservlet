a7ef893425bb1fe38947c382fe9a490c0a89d0b8
baesm971013 <bobbyb537@naver.com>
2024-03-16 20:32:19 +0900

    add ModelView

--- src/main/java/hello/servlet/web/frontcontroller/ModelView.java
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -2,12 +2,11 @@ package hello.servlet.web.frontcontroller;
 
 import java.util.HashMap;
 import java.util.Map;
-
 public class ModelView {
     private String viewName;
-    private Map<String,Object> model = new HashMap<>();
+    private Map<String, Object> model = new HashMap<>();
 
-    public ModelView(String viewName) {
+    public ModelView(String viewName){
         this.viewName = viewName;
     }
 
--- src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -4,5 +4,4 @@ import hello.servlet.web.frontcontroller.ModelView;
 import java.util.Map;
 public interface ControllerV3 {
     ModelView process(Map<String,String> paramMap);
-
 }
